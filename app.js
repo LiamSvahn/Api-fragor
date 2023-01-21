33eb8daf97cbfac651ab0f98325a834488e8f967
liam <01liasyr@skola.boras.se>
2023-01-21 02:29:18 +0100

    nu fungerar det att lägga till en bild ordenttligt och ska fixa en async function

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-"use strict";
-//const divImg = document.querySelector('div') as HTMLDivElement;
-const givesrc = document.querySelector('img');
-console.log("hej");
+'use strict';
+console.log("hej1");
+const givesrc = document.querySelector(".imgsrc");
+console.log("hej2");
 const url = "https://api.thecatapi.com/v1/images/search?limit=10";
 const p = fetch(url)
     .then(response => {
+    console.log("hej3");
     return response.json();
 }).then(catsArray => {
     logCat(catsArray);
 });
-//function gif() {
-//    const gif = new Promise((resolve) => {
-//        setTimeout(() => {
-//            resolve("hej");
-//        }, 1000);
-//    });
-//    return gif;
-//}
 function logCat(cats) {
-    //gif()
     console.log("Cat loaded", cats[1].url);
     givesrc.src = cats[1].url;
 }
--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 'use strict'
-const givesrc = document.querySelector("img") as HTMLImageElement;
+console.log("hej1");
+const givesrc = document.querySelector(".imgsrc") as HTMLImageElement;
 
 
-console.log("hej");
+console.log("hej2");
 const url = "https://api.thecatapi.com/v1/images/search?limit=10";
 
 const p = fetch(url)
     .then(response => {
+        console.log("hej3")
         return response.json();
     }).then(catsArray => {
         logCat(catsArray);
